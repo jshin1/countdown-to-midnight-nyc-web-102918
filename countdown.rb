37f7bdcb95cc7d58f8d5269ff417a5f9b29cca3e
Ji S <j2yshin@gmail.com>
2018-10-14 23:39:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  start = integer
+  while start > 0
+    puts "#{start} SECOND(S)!"
+    start -= 1
+    sleep(60)
+  end
+  "HAPPY NEW YEAR!"
 end
